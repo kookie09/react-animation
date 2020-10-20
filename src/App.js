46d4fec115e52341ebd8796e8070817c36e9cc75
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-10-20 13:38:55 +1100

    use react transition group to apply component transition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -8,7 +9,8 @@ import List from "./components/List/List";
 class App extends Component {
 
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = () => {
@@ -23,10 +25,35 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState(preState => ({ showBlock: !preState.showBlock }))}>
+          Toggle</button>
+        <br />
+
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+          >
+          {state => (
+            <div
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === 'exiting' ? 0 : 1
+              }}></div>)}
+        </Transition>
+
+        { this.state.modalIsOpen ? <Modal
           show={this.state.modalIsOpen}
-          closed={this.closedModal} />
-        <Backdrop />
+          closed={this.closedModal} /> : null}
+        { this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
         <button
           className="Button"
           onClick={this.showModal}>Open Modal</button>
